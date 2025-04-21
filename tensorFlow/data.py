fb5634934e8f7ce0b4610db5f25fc7e32ff42c65
Christoph-eur <christophe.dieder@gmail.com>
2025-04-21 22:15:59 +0200

    TensorFlow CNN
    mise en place d'un réseaux de neurones avec Tensorflow et roboflow pour la base de données

---
+++ tensorFlow/data.py
@@ -0,0 +1,53 @@
+
+# from roboflow import Roboflow
+# rf = Roboflow(api_key="POKrCxImcg3gAbmhpe2W")
+# project = rf.workspace("roboflowrn").project("detection_panneaux")
+# version = project.version(3)
+# dataset = version.download("tfrecord")
+
+from tools import *  
+import tensorflow as tf
+import os
+
+# Load label map
+
+# Obtenir le chemin absolu du fichier .py courant
+base_path = os.path.dirname(__file__)
+label_map_path = os.path.abspath(os.path.join(base_path, '..', 'Detection_panneaux-3', 'test', 'objects_label_map.pbtxt'))
+print("LABEL MAP PATH:", label_map_path)
+label_map = load_label_map('Detection_panneaux-3/test/objects_label_map.pbtxt')#label_map_path
+
+
+# Crée le dataset
+util = Tools("Detection_panneaux-3/train/objects.tfrecord"
+                ,"Detection_panneaux-3/test/objects.tfrecord"
+                ,"Detection_panneaux-3/valid/objects.tfrecord")
+train_dataset = util.train_dataset
+test_dataset = util.test_dataset
+val_dataset = util.val_dataset
+
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Input(shape=(224, 224, 3)),
+    tf.keras.layers.Conv2D(32, 3, activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Conv2D(64, 3, activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(len(label_map), activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_dataset, epochs=10,verbose = 1)
+
+
+# Évaluer le modèle sur les données de test
+loss, accuracy = model.evaluate(test_dataset, batch_size = 64, verbose = 2)
+print(f"Test Loss: {loss}")
+print(f"Test Accuracy: {accuracy}")
+
+model.summary()
