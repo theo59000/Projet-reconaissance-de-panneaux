e1b3545125b692c5eed2172a05c83499edcf85cf
theo59000 <guilmettheo@gmail.com>
2025-05-13 00:32:00 +0200

    train13

---
+++ Detection_panneaux-18/README.dataset.txt
@@ -0,0 +1,7 @@
+# Detection_panneaux > SansMauvaiseImageV2
+https://universe.roboflow.com/roboflowrn/detection_panneaux
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
+the purpose of this project is to create a CNN to recognize traffic signs, focusing on the speed limit signs in France.
--- Yolov8/data.py
+++ Yolov8/data.py
@@ -6,7 +6,8 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.pt")  # build a model pretrain
 
 # Use the model
-model.train(data="C:\\Users\\chris_cp05xbg\\Documents\\GitHub\\Projet-reconaissance-de-panneaux\\Detection_panneaux-15\\data.yaml", epochs=50, patience=10)  # mettre le chemin absolu vers data.yaml
+model.train(data="C:\GitFiles\Projet-reconaissance-de-panneaux\Detection_panneaux-18\data.yaml", epochs=50, patience=5)  # mettre le chemin absolu vers data.yaml
 
 #patience : nombre d'époques sans améliorations avant d'arrêter l'entrainement du réseaux prématurément
 #les poids des arrêtes ainsi que certaines données sur l'entrainement sont stockées dans le dossier run
+                
